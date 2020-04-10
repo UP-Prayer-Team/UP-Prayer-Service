81c39529941d49572745d5b216319659be8f5bd6
Ben Garcia <ben.garcia@meetcircle.com>
2020-04-10 15:02:04 -0700

    Updated deployment info.

--- renew-certs.sh
+++ renew-certs.sh
@@ -9,7 +9,7 @@
 # `sudo certbot certonly -a manual -d api.stage.upmovement.org --email upprayer.staging@gmail.com`
 
 # Combine the PEM files into a PFX with the password
-sudo openssl pkcs12 -inkey /etc/letsencrypt/live/api.stage.upmovement.org/privkey.pem -in /etc/letsencrypt/live/api.stage.upmovement.org/fullchain.pem -export -out UPPrayerService/bin/Debug/netcoreapp3.0/cert.pfx
+#sudo openssl pkcs12 -inkey /etc/letsencrypt/live/api.stage.upmovement.org/privkey.pem -in /etc/letsencrypt/live/api.stage.upmovement.org/fullchain.pem -export -out UPPrayerService/cert.pfx
 
 # Copy the PFX to the staging backend
-scp -r UPPrayerService/bin/Debug/netcoreapp3.0/cert.pfx upd@167.99.162.86:/home/upd/bin/cert.pfx
+scp -r UPPrayerService/cert.pfx upd@167.99.162.86:/home/upd/cert/cert.pfx
