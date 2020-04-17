dd93fa1362905760fb64b05928fa224f1663fa32
Ben Garcia <pokeboygarcia@gmail.com>
2020-04-17 16:11:03 -0700

    Added blogs to the database.

---
+++ UPPrayerService/Models/BlogPost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UPPrayerService.Models
+{
+    public class BlogPost
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public User Author { get; set; }
+        public string Content { get; set; }
+
+        public BlogPost()
+        {
+            this.ID = new Guid().ToString();
+        }
+    }
+}
