bf358235b06c29c946cf3c4da6df252b3f9baf05
Jack Ammon <jammon17@georgefox.edu>
2020-07-09 12:05:22 -0700

    netcore bumped to 3.1 from 3.0

--- deploy-stage.sh
+++ deploy-stage.sh
@@ -1,10 +1,7 @@
 #! /usr/bin/env bash
-
 # Delete the old binaries
 ssh root@api.stage.upmovement.org "rm -r /home/upd/bin"
-
 # Upload the binaries
-scp -r UPPrayerService/bin/Release/netcoreapp3.0/publish/ root@167.99.162.86:/home/upd/bin/
-
+scp -r UPPrayerService/bin/Release/netcoreapp3.1/publish/ root@167.99.162.86:/home/upd/bin/
 # Restart the service
 ssh root@api.stage.upmovement.org "systemctl restart upd"
